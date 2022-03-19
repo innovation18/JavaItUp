a1f6233a161582fd891291de4fe731d690ed91fa
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2022-03-19 21:44:30 +0530

    More Practice Items

--- brushUpJava/src/com/company/MethodOverriding.java
+++ brushUpJava/src/com/company/MethodOverriding.java
@@ -1,21 +1,26 @@
 package com.company;
 
 class Bank {
-   static float getInterestRate () {return 8.0F;}
+   float getInterestRate () {return 8.0F;}
 }
 
 class SBI extends Bank {
-    static float getInterestRate () {return 3.0F;}
+   float getInterestRate () {return 3.0F;}
 }
 
 class ICICI extends Bank {
-   static float getInterestRate () {return 9;}
+   float getInterestRate () {return 9;}
 }
 
 public class MethodOverriding {
     public static void main(String[] args) {
-        System.out.println(ICICI.getInterestRate());
-        System.out.println(SBI.getInterestRate());
+    //    System.out.println(SBI.getInterestRate()); //Instance method 'getInterestRate()' in 'com.company.SBI' cannot override static method 'getInterestRate()' in 'com.company.Bank'
+        Bank sbi;
+
+        sbi = new SBI();
+
+        System.out.println(sbi.getInterestRate());
+
     }
 }
 
