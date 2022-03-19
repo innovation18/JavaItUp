df0c0d7dc7b2138917d07ace4baf23f14de46378
Hanish Arora <61754555+innovation18@users.noreply.github.com>
2022-03-19 21:09:40 +0530

    More Practice Items

--- brushUpJava/src/com/company/SuperUse.java
+++ brushUpJava/src/com/company/SuperUse.java
@@ -3,16 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 class LegalEntity {
-    int connectionId;
-    String legalEntity;
+    protected int connectionId;
+    protected String legalEntity;
+    protected String superKeyword = "Parent Variable";
 
     LegalEntity (int connectionId, String legalEntity) {
         this.connectionId = connectionId;
         this.legalEntity = legalEntity;
     }
 
-}
+    String returnLegalEntity() {
+       return this.legalEntity;
+    }
 
+}
 
 class Connection extends LegalEntity {
 
@@ -20,22 +24,28 @@ class Connection extends LegalEntity {
     int facilityId;
     String legalEntity;
     String sourceSystem;
+    String superKeyword = "Child Variable";
 
     Connection (int connectionId, int customerId, int facilityId, String sourceSystem, String legalEntity) {
         super(connectionId, legalEntity);
 
-        this.connectionId = connectionId;
         this.customerId = customerId;
         this.facilityId = facilityId;
         this.sourceSystem = sourceSystem;
     }
 
     void connectionData() {
-        System.out.println("Connection ID:\t" + this.connectionId + "\n" +
+        System.out.println(
+                "Connection ID:\t" + this.connectionId + "\n" +
                 "Customer ID:\t" + this.customerId + "\n" +
                 "Facility ID:\t" + this.facilityId + "\n" +
                 "Source System:\t" + this.sourceSystem + "\n" +
-                "Legal Entity:\t" + this.legalEntity);
+                "Legal Entity:\t" + returnLegalEntity()
+        );
+
+        System.out.println("This is:\t" + superKeyword);
+
+        System.out.println("This is:\t" + super.superKeyword);
     }
 
     public static void main(String[] args) {
@@ -60,6 +70,9 @@ class Connection extends LegalEntity {
         Connection connection = new Connection(connectionId, customerId, facilityId, sourceSystem, legalEntity);
 
         connection.connectionData();
+
+
+
 
     }
 }
